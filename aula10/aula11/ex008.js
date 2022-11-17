70a69920ef7e8587612ae6b7613e338763933509
GiovanniGoulart <giovanni_goulart@yahoo.com.br>
2022-11-17 13:26:40 -0300

    exercicios aula 10

---
+++ aula10/aula11/ex008.js
@@ -0,0 +1,13 @@
+
+
+
+var vel = 61
+
+console.log (`A veloc do seu carro e ${vel}km/h`)
+
+
+if (vel > 60) {  //Condicao simples!
+    console.log ('Voce ultrapassou a velocidade permitida. Multado!')
+}
+console.log ("Dirija sempre usando cinto de seguranca!")
+
