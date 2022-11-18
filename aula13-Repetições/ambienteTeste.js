a8dd1e269581c9b80e08e42d124d194b39566a20
GiovanniGoulart <giovanni_goulart@yahoo.com.br>
2022-11-18 14:26:38 -0300

    ex. aula 13

---
+++ aula13-Repetições/ambienteTeste.js
@@ -0,0 +1,19 @@
+//Estrutura de repeticao com teste logico no inicio
+
+var c =1
+while (c <= 3) {
+    console.log(`Passo ${c}`)
+    c++                         //c = c+1
+}
+
+
+//Estrutura de repeticao com teste logico no final
+
+/* var c =1
+do  {
+    console.log(`Passo ${c}`)
+    c++                             
+} while (c <= 100
+    )
+*/
+
